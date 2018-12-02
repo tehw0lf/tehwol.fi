a6e70d8aace2b422e551edda173a6bdc067c085b
tehw0lf <tehwolf@protonmail.com>
2018-12-02 14:44:34 +0100

    Add activity indicator to nav links
    The links in the navigation bar now show the active route

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
@@ -6,7 +7,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  isActive(): boolean {
+    return (
+      this.router.isActive("/", true) || this.router.isActive("/home", true)
+    );
+  }
 }
