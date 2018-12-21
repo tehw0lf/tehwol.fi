28915300b05bc300502922d1bfdda06a50faf0ee
tehw0lf <tehwolf@protonmail.com>
2018-12-21 22:10:36 +0100

    Add portfolio
    This adds the github repository portfolio feature.
    
    Resolves #8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent }
 ];
 
 @NgModule({
