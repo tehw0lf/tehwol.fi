3c56c9383f424677e77393f7bc11fd692248d321
tehw0lf <tehwolf@protonmail.com>
2019-07-14 13:57:45 +0200

    Update packages
    Nothing new, just updates

--- src/app/nav/mobile/mobile.component.ts
+++ src/app/nav/mobile/mobile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material';
+import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { SidenavService } from 'src/app/nav/sidenav.service';
 
