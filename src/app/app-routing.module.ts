5f502eeb9d934648b8a68a7a52060a887c4c0f48
tehw0lf <tehwolf@protonmail.com>
2020-06-15 00:10:01 +0200

    Add wordlist generator
    This is merely a proof of concept, the user input is still missing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WordlistGeneratorComponent } from './wordlist-generator/wordlist-generator.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'portfolio', component: PortfolioComponent }
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'wordlist-generator', component: WordlistGeneratorComponent }
 ];
 
 @NgModule({
