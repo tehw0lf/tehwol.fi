7f3eb317850fdb550565c945d8e31ea3d7414ac0
tehw0lf <tehwolf@protonmail.com>
2020-06-29 21:09:11 +0200

    Add first e2e test
    Yay!

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,5 +1,5 @@
 it("loads examples", () => {
-  const baseUrl: string = "http://localhost:4200";
+  const baseUrl = "http://localhost:4200";
   cy.visit(baseUrl);
-  cy.contains("Replace me with something relevant");
+  cy.contains("Welcome to tehwol.fi!");
 });
