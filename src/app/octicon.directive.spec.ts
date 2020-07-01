c1031f98447fcf2242c73e48b68a14f47be359e6
tehw0lf <tehwolf@protonmail.com>
2020-07-01 19:12:47 +0200

    Remove unneeded assertion
    We don't need to check the stringified object if we can check props

--- src/app/octicon.directive.spec.ts
+++ src/app/octicon.directive.spec.ts
@@ -1,14 +1,11 @@
 import { Component, DebugElement, Renderer2, Type } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import * as octicons from 'octicons';
 
 import { OcticonDirective } from './octicon.directive';
 
 @Component({
-  template: `
-    <div octicon="star" color="gold"></div>
-  `
+  template: ` <div octicon="star" color="gold"></div> `
 })
 class TestOcticonDirectiveComponent {}
 
@@ -18,7 +15,6 @@ describe('OcticonDirective', () => {
   let inputEl: DebugElement;
   let renderer: Renderer2;
   let rendererSpy: any;
-  let star: string;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -27,9 +23,9 @@ describe('OcticonDirective', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(TestOcticonDirectiveComponent);
-    renderer = fixture.componentRef.injector.get<Renderer2>(Renderer2 as Type<
-      Renderer2
-    >);
+    renderer = fixture.componentRef.injector.get<Renderer2>(
+      Renderer2 as Type<Renderer2>
+    );
     rendererSpy = jest.spyOn(renderer, 'setStyle');
     component = fixture.componentInstance;
     inputEl = fixture.debugElement.query(By.css('div'));
@@ -38,11 +34,8 @@ describe('OcticonDirective', () => {
   });
 
   it('should create a div with golden octicon star svg as innerHTML', () => {
-    star = octicons['star'].toSVG().replace('/>', '></path>');
-
     expect(rendererSpy).toHaveBeenCalled();
     expect(inputEl.attributes.color).toBe('gold');
     expect(inputEl.attributes.octicon).toBe('star');
-    expect(inputEl.nativeElement.innerHTML).toBe(star);
   });
 });
