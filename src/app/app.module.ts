783e88b473e1bd515b23b19aac539099075dbbf3
tehw0lf <tehwolf@protonmail.com>
2020-07-16 22:09:37 +0200

    Add material design
    And subscribe for debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -49,6 +50,7 @@ import { WordlistGeneratorComponent } from './wordlist-generator/wordlist-genera
     HttpClientModule,
     MatCardModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatToolbarModule,
     MatSidenavModule,
