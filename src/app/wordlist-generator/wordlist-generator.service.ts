5690762860d7e95f8e61235d6d9e978987b1a607
tehw0lf <tehwolf@protonmail.com>
2020-07-17 03:14:41 +0200

    Switch to ReplaySubject

--- src/app/wordlist-generator/wordlist-generator.service.ts
+++ src/app/wordlist-generator/wordlist-generator.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { product } from 'cartesian-product-generator';
-import { Observable, of } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordlistGeneratorService {
-  private wordlist$: Observable<IterableIterator<string[]>>;
+  private wordlist$: ReplaySubject<IterableIterator<string[]>>;
   constructor() {}
 
   generateWordlist(...charsets: string[]): void {
-    this.wordlist$ = of(product(...charsets));
+    if (this.wordlist$ === undefined) {
+      this.wordlist$ = new ReplaySubject();
+    }
+    console.log('next value:', ...charsets);
+    this.wordlist$.next(product(...charsets));
   }
 
   getWordlist(): Observable<IterableIterator<string[]>> {
-    return this.wordlist$;
+    return this.wordlist$.asObservable();
   }
 }
