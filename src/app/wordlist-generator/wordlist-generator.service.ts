4fa385249e9608877d5bb9ebb0c53b9d6401d17b
tehw0lf <tehwolf@protonmail.com>
2020-07-25 19:26:03 +0200

    Switch to buffer array
    Now our output is persistent

--- src/app/wordlist-generator/wordlist-generator.service.ts
+++ src/app/wordlist-generator/wordlist-generator.service.ts
@@ -1,23 +1,16 @@
 import { Injectable } from '@angular/core';
 import { product } from 'cartesian-product-generator';
-import { Observable, ReplaySubject } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordlistGeneratorService {
-  private wordlist$: ReplaySubject<IterableIterator<string[]>>;
   constructor() {}
 
-  generateWordlist(...charsets: string[]): void {
-    if (this.wordlist$ === undefined) {
-      this.wordlist$ = new ReplaySubject();
-    }
-    console.log('next value:', ...charsets);
-    this.wordlist$.next(product(...charsets));
-  }
-
-  getWordlist(): Observable<IterableIterator<string[]>> {
-    return this.wordlist$.asObservable();
+  generateWordlist(
+    ...charsets: string[]
+  ): Observable<IterableIterator<string[]>> {
+    return of(product(...charsets));
   }
 }
