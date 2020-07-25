387cbef3da77d4aff298564f5dc3da6486e5b41c
tehw0lf <tehwolf@protonmail.com>
2020-07-25 23:38:46 +0200

    Fix test

--- src/app/wordlist-generator/wordlist-generator.component.spec.ts
+++ src/app/wordlist-generator/wordlist-generator.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { WordlistGeneratorComponent } from './wordlist-generator.component';
 
@@ -8,7 +12,13 @@ describe('WordlistGeneratorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [WordlistGeneratorComponent]
+      declarations: [WordlistGeneratorComponent],
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule
+      ]
     }).compileComponents();
   }));
 
