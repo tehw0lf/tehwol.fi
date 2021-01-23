ec3cc58d2c184f268a766fd97d43fd697ab51930
tehw0lf <tehwolf@protonmail.com>
2021-01-23 03:42:19 +0100

    Fix module and lint

--- projects/git-portfolio/src/lib/git-portfolio.module.ts
+++ projects/git-portfolio/src/lib/git-portfolio.module.ts
@@ -1,10 +1,12 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 import { GitPortfolioComponent } from './git-portfolio.component';
 
 @NgModule({
   declarations: [GitPortfolioComponent],
-  imports: [],
+  imports: [MatCardModule, ClipboardModule],
   exports: [GitPortfolioComponent]
 })
 export class GitPortfolioModule {}
