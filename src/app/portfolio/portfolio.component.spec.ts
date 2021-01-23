2d9589c91b189b0818a0947060639513a27d35c5
tehw0lf <tehwolf@protonmail.com>
2021-01-23 03:42:40 +0100

    Fix module and import

--- src/app/portfolio/portfolio.component.spec.ts
+++ src/app/portfolio/portfolio.component.spec.ts
@@ -1,21 +1,10 @@
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
-import { of } from 'rxjs';
 
 import { GitPortfolioComponent } from '../../../projects/git-portfolio/src/lib/git-portfolio.component';
-import { GitProviderService } from '../../../projects/git-portfolio/src/lib/git-provider.service';
-import { GitRepository } from '../../../projects/git-portfolio/src/lib/git-repository-type';
 import { RepoCardComponent } from '../../../projects/git-portfolio/src/lib/repo-card/repo-card.component';
 import { PortfolioComponent } from './portfolio.component';
-
-const GIT_REPO = new GitRepository();
-
-const gitProviderServiceStub = {
-  fetchRepositories() {
-    return of([GIT_REPO]);
-  }
-};
 
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
@@ -26,12 +15,9 @@ describe('PortfolioComponent', () => {
       TestBed.configureTestingModule({
         imports: [MatCardModule, ClipboardModule],
         declarations: [
-          PortfolioComponent,
           GitPortfolioComponent,
+          PortfolioComponent,
           RepoCardComponent
-        ],
-        providers: [
-          { provide: GitProviderService, useValue: gitProviderServiceStub }
         ]
       }).compileComponents();
     })
