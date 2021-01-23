0ec2d074776355afa2f3360f29a8e166dbd5a804
tehw0lf <tehwolf@protonmail.com>
2021-01-23 04:21:51 +0100

    Update imports
    not working yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
-import { GitPortfolioComponent, RepoCardComponent } from '../../dist/git-portfolio/public-api';
+import { GitPortfolioComponent, RepoCardComponent } from '../../dist/git-portfolio/git-portfolio';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
