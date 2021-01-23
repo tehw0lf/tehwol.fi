d140848625e0f3eaec69674790d9136c1dad4a02
tehw0lf <tehwolf@protonmail.com>
2021-01-23 04:21:39 +0100

    Update config

--- projects/git-portfolio/jest.config.js
+++ projects/git-portfolio/jest.config.js
@@ -1,9 +1,5 @@
+const baseConfig = require('../../jest.base.config');
+
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/']
+  ...baseConfig
 };
