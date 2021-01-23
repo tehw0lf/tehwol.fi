7a25285aae094be62318fe28360b83bcc760f0e0
tehw0lf <tehwolf@protonmail.com>
2021-01-23 03:42:19 +0100

    Fix module and lint

--- projects/git-portfolio/src/lib/git-portfolio.component.spec.ts
+++ projects/git-portfolio/src/lib/git-portfolio.component.spec.ts
@@ -11,7 +11,7 @@ import { RepoCardComponent } from './repo-card/repo-card.component';
 const GIT_REPO = new GitRepository();
 
 const gitProviderServiceStub = {
-  fetchRepositories() {
+  fetchRepositories: () => {
     return of([GIT_REPO]);
   }
 };
