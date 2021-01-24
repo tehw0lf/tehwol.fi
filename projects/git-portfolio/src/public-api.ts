3450b51c1d72e046131bc88f81a72bea0b7fd426
tehw0lf <tehwolf@protonmail.com>
2021-01-24 01:48:43 +0100

    Remove project from tehwol.fi

--- projects/git-portfolio/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of git-portfolio
- */
-
-export * from './lib/git-portfolio.component';
-export * from './lib/git-portfolio.module';
-export { GitProviders } from './lib/git-repositories-type';
-export * from './lib/repo-card/repo-card.component';
