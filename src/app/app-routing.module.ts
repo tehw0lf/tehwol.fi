1c93617adf633f8634d91e1fe17cc0eaf465ecb0
tehw0lf <tehwolf@protonmail.com>
2021-03-13 00:09:37 +0100

    Refactor Portfolio -> Git-Portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { GitPortfolioComponent } from './git-portfolio/git-portfolio.component';
 import { HomeComponent } from './home/home.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { WordlistGeneratorComponent } from './wordlist-generator/wordlist-generator.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'wordlist-generator', component: WordlistGeneratorComponent }
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "portfolio", component: GitPortfolioComponent },
+  { path: "wordlist-generator", component: WordlistGeneratorComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
