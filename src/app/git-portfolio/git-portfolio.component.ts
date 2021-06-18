08651d325b390bcd63be5bc74f0cdd27e17d99bb
tehw0lf <tehwolf@protonmail.com>
2021-06-18 23:22:54 +0200

    Use privacy dialog on portfolio page
    To inform our users that we call the public GitHub API

--- src/app/git-portfolio/git-portfolio.component.ts
+++ src/app/git-portfolio/git-portfolio.component.ts
@@ -1,19 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment.prod';
+import { PrivacyDialogComponent } from '../privacy-dialog/privacy-dialog.component';
 
 @Component({
-  selector: "app-git-portfolio",
-  templateUrl: "./git-portfolio.component.html",
-  styleUrls: ["./git-portfolio.component.scss"],
+  selector: 'app-git-portfolio',
+  templateUrl: './git-portfolio.component.html',
+  styleUrls: ['./git-portfolio.component.scss']
 })
 export class GitPortfolioComponent implements OnInit {
   gitProviderConfig: { github: string; gitlab: string } = {
     github: environment.githubUser,
-    gitlab: environment.gitlabUser,
+    gitlab: environment.gitlabUser
   };
+  cachedPrivacyDecision = false;
+  privacyNoticeAccepted: Observable<boolean>;
 
-  constructor() {}
+  constructor(private dialog: MatDialog) {
+    this.cachedPrivacyDecision =
+      window.localStorage.getItem('privacyNoticeAccepted') === 'true';
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.cachedPrivacyDecision) {
+      this.createPrivacyDialog();
+    } else {
+      this.privacyNoticeAccepted = of(true);
+    }
+  }
+
+  private createPrivacyDialog(): void {
+    const privacyDialogRef: MatDialogRef<PrivacyDialogComponent, boolean> =
+      this.dialog.open(PrivacyDialogComponent);
+    this.privacyNoticeAccepted = privacyDialogRef
+      .afterClosed()
+      .pipe(tap((decision) => this.storePrivacyDecision(decision)));
+  }
+
+  private storePrivacyDecision(decision: boolean): void {
+    window.localStorage.setItem('privacyNoticeAccepted', decision.toString());
+  }
 }
