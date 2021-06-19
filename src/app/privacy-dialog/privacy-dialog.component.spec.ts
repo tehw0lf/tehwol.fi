fd2f92e19c0ce61f412ed314b463fb3d97798685
tehw0lf <tehwolf@protonmail.com>
2021-06-19 18:36:21 +0200

    Test privacy dialog

--- src/app/privacy-dialog/privacy-dialog.component.spec.ts
+++ src/app/privacy-dialog/privacy-dialog.component.spec.ts
@@ -1,16 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { of } from 'rxjs';
 
 import { PrivacyDialogComponent } from './privacy-dialog.component';
 
+const mockDialogRef = {
+  close: jest.fn()
+};
+
+const mockMatDialog = {
+  close: (value) => {
+    return {
+      afterClosed: () => of(value)
+    };
+  }
+};
 describe('PrivacyDialogComponent', () => {
   let component: PrivacyDialogComponent;
   let fixture: ComponentFixture<PrivacyDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PrivacyDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [PrivacyDialogComponent],
+      imports: [MatDialogModule],
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MatDialog, useValue: mockMatDialog }
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +38,15 @@ describe('PrivacyDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return true on accept', () => {
+    component.accept();
+    expect(mockDialogRef.close).toHaveBeenCalledWith(true);
+  });
+
+  it('should return false on deny', () => {
+    component.deny();
+    expect(mockDialogRef.close).toHaveBeenCalledWith(false);
   });
 });
