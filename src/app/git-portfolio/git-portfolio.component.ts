106ab840d2db5421518c164e71e7755960eb4426
tehw0lf <tehwolf@protonmail.com>
2021-06-19 21:32:20 +0200

    Extend tests for portfolio

--- src/app/git-portfolio/git-portfolio.component.ts
+++ src/app/git-portfolio/git-portfolio.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment.prod';
 import { PrivacyDialogComponent } from '../privacy-dialog/privacy-dialog.component';
@@ -11,20 +11,21 @@ import { PrivacyDialogComponent } from '../privacy-dialog/privacy-dialog.compone
   templateUrl: './git-portfolio.component.html',
   styleUrls: ['./git-portfolio.component.scss']
 })
-export class GitPortfolioComponent implements OnInit {
+export class GitPortfolioComponent implements OnInit, OnDestroy {
   gitProviderConfig: { github: string; gitlab: string } = {
     github: environment.githubUser,
     gitlab: environment.gitlabUser
   };
   cachedPrivacyDecision = false;
   privacyNoticeAccepted: Observable<boolean>;
+  unsubscribe$ = new Subject<void>();
 
-  constructor(private dialog: MatDialog) {
-    this.cachedPrivacyDecision =
-      window.localStorage.getItem('privacyNoticeAccepted') === 'true';
-  }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.cachedPrivacyDecision =
+      window.localStorage.getItem('privacyNoticeAccepted') === 'true';
+
     if (!this.cachedPrivacyDecision) {
       this.createPrivacyDialog();
     } else {
@@ -32,15 +33,26 @@ export class GitPortfolioComponent implements OnInit {
     }
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
   private createPrivacyDialog(): void {
     const privacyDialogRef: MatDialogRef<PrivacyDialogComponent, boolean> =
       this.dialog.open(PrivacyDialogComponent);
-    this.privacyNoticeAccepted = privacyDialogRef
-      .afterClosed()
-      .pipe(tap((decision) => this.storePrivacyDecision(decision)));
-  }
-
-  private storePrivacyDecision(decision: boolean): void {
-    window.localStorage.setItem('privacyNoticeAccepted', decision.toString());
+    this.privacyNoticeAccepted = privacyDialogRef.afterClosed().pipe(
+      tap((decision) => {
+        if (decision === true) {
+          window.localStorage.setItem(
+            'privacyNoticeAccepted',
+            decision.toString()
+          );
+        } else {
+          window.history.back();
+        }
+      }),
+      takeUntil(this.unsubscribe$)
+    );
   }
 }
