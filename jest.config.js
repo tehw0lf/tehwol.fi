b6b6db542c8f37c8b57d671acc66b2d5a6b1970f
tehw0lf <tehwolf@protonmail.com>
2021-07-17 01:06:29 +0200

    Add boilerplate

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   projects: [
     '<rootDir>/apps/tehwolfi',
     '<rootDir>/libs/ng-git-portfolio',
-    '<rootDir>/libs/ng-wordlist-generator'
+    '<rootDir>/libs/ng-wordlist-generator',
+    '<rootDir>/libs/airbash-database-editor'
   ]
 };
