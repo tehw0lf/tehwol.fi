10ccd2254554c534d316c2d10eacc755764eeaf6
tehw0lf <tehwolf@protonmail.com>
2021-07-17 01:11:18 +0200

    Add packages and imports

--- libs/airbash-database-editor/src/lib/airbash-database-editor.module.ts
+++ libs/airbash-database-editor/src/lib/airbash-database-editor.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,]
 })
 export class AirbashDatabaseEditorModule {}
