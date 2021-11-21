04778192f98ec713ac745c278be84e0036989142
tehw0lf <tehwolf@protonmail.com>
2021-11-21 18:40:03 +0100

    Add new wordlist generator library

--- libs/wordlist-generator/src/lib/wordlist-generator.module.ts
+++ libs/wordlist-generator/src/lib/wordlist-generator.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { WordlistGeneratorComponent } from './wordlist-generator.component';
+import { WordlistGeneratorService } from './wordlist-generator.service';
 
 @NgModule({
-  imports: [CommonModule]
+  declarations: [WordlistGeneratorComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    ReactiveFormsModule,
+    ClipboardModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
+  exports: [WordlistGeneratorComponent],
+  providers: [WordlistGeneratorService]
 })
 export class WordlistGeneratorModule {}
