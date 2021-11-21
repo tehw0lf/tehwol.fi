f0c8dbdf52823f22ecfc95b9ef388eba9ffafee1
tehw0lf <tehwolf@protonmail.com>
2021-11-21 18:39:51 +0100

    Add new git portfolio library

--- libs/git-portfolio/src/lib/git-portfolio.module.ts
+++ libs/git-portfolio/src/lib/git-portfolio.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GitPortfolioComponent } from './git-portfolio.component';
+import { GitProviderService } from './git-provider.service';
+import { OcticonDirective } from './octicon.directive';
+import { RepoCardComponent } from './repo-card/repo-card.component';
 
 @NgModule({
-  imports: [CommonModule]
+  declarations: [GitPortfolioComponent, RepoCardComponent, OcticonDirective],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    ClipboardModule,
+    FlexLayoutModule
+  ],
+  exports: [GitPortfolioComponent, OcticonDirective],
+  providers: [GitProviderService]
 })
 export class GitPortfolioModule {}
