3233370241dedd4f0a4de1cc78f957ce04e41a55
tehw0lf <tehwolf@protonmail.com>
2021-12-07 21:52:53 +0100

    Add contact form component

---
+++ libs/contact-form/src/lib/contact-form/contact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tehw0lf-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss']
+})
+export class ContactFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
