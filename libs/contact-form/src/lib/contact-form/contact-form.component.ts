d28646b6ab180d3a344c2cf73fe9357da14be300
tehw0lf <tehwolf@protonmail.com>
2021-12-08 23:29:47 +0100

    Extend contact-form library

--- libs/contact-form/src/lib/contact-form/contact-form.component.ts
+++ libs/contact-form/src/lib/contact-form/contact-form.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { tap } from 'rxjs/operators';
+
+import { EmailApiService } from '../email-api.service';
 
 @Component({
-  selector: 'tehw0lf-contact-form',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
+  @Input() apiURL = '';
 
-  constructor() { }
+  formGroup: FormGroup;
+  callResult = '';
 
-  ngOnInit(): void {
+  constructor(
+    private builder: FormBuilder,
+    private emailService: EmailApiService
+  ) {
+    this.formGroup = this.builder.group({
+      from: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', [Validators.required]),
+      subject: new FormControl(''),
+      message: new FormControl('', [Validators.required])
+    });
   }
 
+  submitFormData(formData: FormGroup) {
+    this.emailService
+      .sendEmail(this.apiURL, formData.value)
+      .pipe(
+        tap((errorStatusText?: string) => {
+          if (!errorStatusText) {
+            this.callResult = 'E-Mail erfolgreich versendet!';
+          } else {
+            this.callResult = `Error: ${errorStatusText}`;
+          }
+        })
+      )
+      .subscribe();
+  }
 }
--- libs/contact-form/src/lib/email-api.service.ts
+++ libs/contact-form/src/lib/email-api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  sendEmail(apiURL: string, data: any): Observable<any> {
+    return this.http.post(apiURL, data).pipe(
+      catchError((error: any) => {
+        return of(error.statusText);
+      })
+    );
+  }
 }
