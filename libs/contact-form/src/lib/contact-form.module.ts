bbfbf328ac7e383f5a031dfbc442fa55d3ec1c72
tehw0lf <tehwolf@protonmail.com>
2021-12-08 23:29:47 +0100

    Extend contact-form library

--- libs/contact-form/src/lib/contact-form.module.ts
+++ libs/contact-form/src/lib/contact-form.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { EmailApiService } from './email-api.service';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    ContactFormComponent
-  ]
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [ContactFormComponent],
+  exports: [ContactFormComponent],
+  providers: [EmailApiService],
 })
 export class ContactFormModule {}
