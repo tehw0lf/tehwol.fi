38e8ed09477947348d3d7a7dee95368bd0f6b985
tehw0lf <tehwolf@protonmail.com>
2021-12-09 17:03:48 +0100

    Add header and JSON.stringify data

--- libs/contact-form/src/lib/email-api.service.ts
+++ libs/contact-form/src/lib/email-api.service.ts
@@ -1,16 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailApiService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
   constructor(private http: HttpClient) {}
 
   sendEmail(apiURL: string, data: any): Observable<any> {
-    return this.http.post(apiURL, data).pipe(
+    return this.http.post(apiURL, JSON.stringify(data), this.httpOptions).pipe(
+      map((response: any) => {
+        return response;
+      }),
       catchError((error: any) => {
         return of(error.statusText);
       })
