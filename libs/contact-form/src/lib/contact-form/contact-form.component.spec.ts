a44435ebdb1dc0d3817954b628a318383ee663b3
tehw0lf <tehwolf@protonmail.com>
2021-12-09 17:43:47 +0100

    Add module imports to spec files

--- libs/contact-form/src/lib/contact-form/contact-form.component.spec.ts
+++ libs/contact-form/src/lib/contact-form/contact-form.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactFormComponent } from './contact-form.component';
 
@@ -8,9 +10,9 @@ describe('ContactFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactFormComponent ]
-    })
-    .compileComponents();
+      declarations: [ContactFormComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule]
+    }).compileComponents();
   });
 
   beforeEach(() => {
