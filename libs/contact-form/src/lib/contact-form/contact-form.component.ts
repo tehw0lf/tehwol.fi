1be53d05a1988c52a9566f56f809e5d994e7c6e2
tehw0lf <tehwolf@protonmail.com>
2021-12-09 17:05:13 +0100

    Prevent memory leak, fix status message

--- libs/contact-form/src/lib/contact-form/contact-form.component.ts
+++ libs/contact-form/src/lib/contact-form/contact-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 import { EmailApiService } from '../email-api.service';
 
@@ -10,35 +11,44 @@ import { EmailApiService } from '../email-api.service';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent {
-  @Input() apiURL = '';
+export class ContactFormComponent implements OnDestroy {
+  @Input() apiURL = 'https://forwardmethis.com/tehwolf@pm.me';
 
   formGroup: FormGroup;
-  callResult = '';
+  emailSent: Subject<boolean | null> = new Subject();
+  emailSent$ = this.emailSent.asObservable();
+
+  private unsubscribe$: Subject<void> = new Subject();
 
   constructor(
     private builder: FormBuilder,
     private emailService: EmailApiService
   ) {
+    this.emailSent.next(null);
     this.formGroup = this.builder.group({
-      from: new FormControl('', [Validators.required, Validators.email]),
       name: new FormControl('', [Validators.required]),
-      subject: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
       message: new FormControl('', [Validators.required])
     });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   submitFormData(formData: FormGroup) {
     this.emailService
       .sendEmail(this.apiURL, formData.value)
       .pipe(
-        tap((errorStatusText?: string) => {
-          if (!errorStatusText) {
-            this.callResult = 'E-Mail erfolgreich versendet!';
+        tap((response: string) => {
+          if (response === 'OK') {
+            this.emailSent.next(true);
           } else {
-            this.callResult = `Error: ${errorStatusText}`;
+            this.emailSent.next(false);
           }
-        })
+        }),
+        takeUntil(this.unsubscribe$)
       )
       .subscribe();
   }
