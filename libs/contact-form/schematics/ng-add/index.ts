4269bba8a4abece9b6b2895ab33869aa3e5d338e
tehw0lf <tehwolf@protonmail.com>
2021-12-11 23:58:26 +0100

    Add schematic for app module entry

---
+++ libs/contact-form/schematics/ng-add/index.ts
@@ -0,0 +1,44 @@
+import { chain, Rule, Tree } from '@angular-devkit/schematics';
+import {
+  addModuleImportToRootModule,
+  getAppModulePath,
+  getProjectFromWorkspace,
+  getProjectMainFile,
+  hasNgModuleImport
+} from '@angular/cdk/schematics';
+import { getWorkspace } from '@schematics/angular/utility/workspace';
+import { ProjectType } from '@schematics/angular/utility/workspace-models';
+
+import { Schema } from './schema';
+
+const contactFormModuleName = 'ContactFormModule';
+const contactFormPackageName = '@tehw0lf/contact-form';
+
+export default function (options: Schema): Rule {
+  return async (host: Tree) => {
+    const workspace = await getWorkspace(host);
+    const project = getProjectFromWorkspace(workspace, options.project);
+
+    if (project.extensions.projectType === ProjectType.Application) {
+      return chain([addContactFormModule(options)]);
+    }
+    return;
+  };
+}
+
+function addContactFormModule(options: Schema) {
+  return async (host: Tree) => {
+    const workspace = await getWorkspace(host);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    const appModulePath = getAppModulePath(host, getProjectMainFile(project));
+
+    if (!hasNgModuleImport(host, appModulePath, contactFormModuleName)) {
+      addModuleImportToRootModule(
+        host,
+        contactFormModuleName,
+        contactFormPackageName,
+        project
+      );
+    }
+  };
+}
