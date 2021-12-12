3747c9c77f61ad481233fab20bca076c9e818a98
tehw0lf <tehwolf@protonmail.com>
2021-12-12 17:45:01 +0100

    Expand schematic
    And bump package version

--- libs/contact-form/schematics/ng-add/index.ts
+++ libs/contact-form/schematics/ng-add/index.ts
@@ -1,23 +1,54 @@
 import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import {
-    addModuleImportToRootModule,
-    getAppModulePath,
-    getProjectFromWorkspace,
-    getProjectMainFile,
-    hasNgModuleImport
+  addModuleImportToRootModule,
+  getAppModulePath,
+  getProjectFromWorkspace,
+  getProjectMainFile,
+  hasNgModuleImport
 } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import { ProjectType } from '@schematics/angular/utility/workspace-models';
 
+import { addPackageToPackageJson, getPackageVersionFromPackageJson } from './package-config';
 import { Schema } from './schema';
 
 const contactFormModuleName = 'ContactFormModule';
 const contactFormPackageName = '@tehw0lf/contact-form';
+const flexLayoutFallbackVersion = '^13.0.0-beta.36';
 
 export default function (options: Schema): Rule {
   return async (host: Tree, context: SchematicContext) => {
     const workspace = await getWorkspace(host);
     const project = getProjectFromWorkspace(workspace, options.project);
+
+    const coreVersion = getPackageVersionFromPackageJson(host, '@angular/core');
+    const flexLayoutVersion = getPackageVersionFromPackageJson(
+      host,
+      '@angular/flex-layout'
+    );
+    const materialVersion = getPackageVersionFromPackageJson(
+      host,
+      '@angular/material'
+    );
+    const dependencyVersion = coreVersion || '0.0.0';
+
+    if (!flexLayoutVersion || flexLayoutVersion !== flexLayoutFallbackVersion) {
+      addPackageToPackageJson(
+        host,
+        '@angular/flex-layout',
+        flexLayoutFallbackVersion
+      );
+
+      context.logger.info(
+        `@angular/flex-layout ${flexLayoutFallbackVersion} was added to dependencies.`
+      );
+    }
+
+    if (!materialVersion || materialVersion !== coreVersion) {
+      context.logger.error(`@angular/material ${dependencyVersion} not found.
+       Please run 'ng add @angular/material' first`);
+      return;
+    }
 
     if (project.extensions.projectType === ProjectType.Application) {
       return chain([addContactFormModule(options)]);
