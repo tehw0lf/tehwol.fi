b2306638d0d950df3a8810573137e22c114b80fb
tehw0lf <tehwolf@protonmail.com>
2021-12-12 21:58:10 +0100

    Test with and without material present

--- libs/contact-form/schematics/ng-add/index.spec.ts
+++ libs/contact-form/schematics/ng-add/index.spec.ts
@@ -2,10 +2,11 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 
 import { getFileContent } from '../testing/file-content';
-import { createTestApp } from '../testing/test-app';
+import { createTestApp, createTestAppWithMaterial } from '../testing/test-app';
 import { createTestLibrary } from '../testing/test-library';
+import { getPackageVersionFromPackageJson } from './package-config';
 
-describe('ng-add schematic', () => {
+describe('ng-add schematic with material present', () => {
   let runner: SchematicTestRunner;
   let appTree: Tree;
   let errorOutput: string[];
@@ -16,7 +17,7 @@ describe('ng-add schematic', () => {
       'schematics',
       require.resolve('../collection.json')
     );
-    appTree = await createTestApp(runner);
+    appTree = await createTestAppWithMaterial(runner);
 
     errorOutput = [];
     warnOutput = [];
@@ -40,6 +41,57 @@ describe('ng-add schematic', () => {
       );
 
       expect(fileContent).toContain('ContactFormModule');
+    });
+  });
+});
+
+describe('ng-add schematic without material present', () => {
+  let runner: SchematicTestRunner;
+  let appTree: Tree;
+  let errorOutput: string[];
+  let warnOutput: string[];
+
+  beforeEach(async () => {
+    runner = new SchematicTestRunner(
+      'schematics',
+      require.resolve('../collection.json')
+    );
+    appTree = await createTestApp(runner);
+
+    errorOutput = [];
+    warnOutput = [];
+    runner.logger.subscribe((e) => {
+      if (e.level === 'error') {
+        errorOutput.push(e.message);
+      } else if (e.level === 'warn') {
+        warnOutput.push(e.message);
+      }
+    });
+  });
+
+  describe('fail to add module', () => {
+    it('should fail if material is missing', async () => {
+      const tree = await runner
+        .runSchematicAsync('ng-add', {}, appTree)
+        .toPromise();
+
+      const fileContent = getFileContent(
+        tree,
+        '/projects/material/src/app/app.module.ts'
+      );
+
+      const coreVersion = getPackageVersionFromPackageJson(
+        tree,
+        '@angular/core'
+      );
+
+      expect(fileContent).not.toContain('ContactFormModule');
+
+      expect(errorOutput.length).toBe(1);
+      expect(warnOutput.length).toBe(0);
+      expect(errorOutput[0])
+        .toMatch(`@angular/material ${coreVersion} not found.
+       Please run 'ng add @angular/material' first`);
     });
   });
 });
@@ -71,8 +123,9 @@ describe('ng-add schematic - library project', () => {
   it('should do nothing if a library project is targeted', async () => {
     await runner.runSchematicAsync('ng-add', {}, libraryTree).toPromise();
 
-    expect(errorOutput.length).toBe(0);
-    expect(warnOutput.length).toBe(1);
-    expect(warnOutput[0]).toMatch('Please specify an application project');
+    expect(errorOutput.length).toBe(1);
+    expect(errorOutput[0]).toMatch(
+      'This library needs to be added to an application project'
+    );
   });
 });
