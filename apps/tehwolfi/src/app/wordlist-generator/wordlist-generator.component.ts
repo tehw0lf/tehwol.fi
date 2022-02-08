cfdaa890419b8c0355ed8fcd36dbf07ac0bf285c
tehw0lf <tehwolf@protonmail.com>
2022-02-08 17:52:16 +0100

    Refactor styling

--- apps/tehwolfi/src/app/wordlist-generator/wordlist-generator.component.ts
+++ apps/tehwolfi/src/app/wordlist-generator/wordlist-generator.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Subject, takeUntil, tap } from 'rxjs';
+
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'tehw0lf-wordlist-generator',
@@ -6,11 +9,31 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./wordlist-generator.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class WordlistGeneratorComponent {
+export class WordlistGeneratorComponent implements OnInit, OnDestroy {
   buttonBackgroundColor = 'rgba(34, 34, 34, 0.75)';
+  buttonStyle: string[] = [
+    `"background-color": "${this.buttonBackgroundColor}"`,
+    'color: "#cc7832"'
+  ];
 
-  constructor() {
-    //
+  private unsubscribe$: Subject<void> = new Subject();
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.isLight
+      .pipe(
+        tap((isLight: boolean) => {
+          isLight ? this.switchToLight() : this.switchToDark();
+        }),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   switchToLight(): void {
