51a78b23a6cb6f0a313ce6f1525de7a0eebb66f2
tehw0lf <tehwolf@protonmail.com>
2022-02-12 00:39:07 +0100

    Add Contact Form Component

---
+++ apps/tehwolfi/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,92 @@
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil, tap } from 'rxjs';
+
+import { ThemeService } from '../theme.service';
+
+@Component({
+  selector: 'tehw0lf-contact-form',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss']
+})
+export class ContactFormComponent implements OnInit, OnDestroy {
+  buttonStyle = {
+    'background-color': '#424242',
+    border: '1px solid #ced4da',
+    color: '#cc7832'
+  };
+
+  formStyle = {
+    color: '#437da8',
+    'background-color': 'rgba(34, 34, 34, 0.75)',
+    'backdrop-filter': 'blur(50px)',
+    'box-shadow': '0 2px 10px rgba(0, 0, 0, 0.075)'
+  };
+
+  inputStyle = {
+    color: '#282b2e',
+    'background-color': '#fff',
+    border: '1px solid #ced4da'
+  };
+  private unsubscribe$: Subject<void> = new Subject();
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.isLight
+      .pipe(
+        tap((isLight: boolean) => {
+          isLight ? this.switchToLight() : this.switchToDark();
+        }),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  switchToLight(): void {
+    this.buttonStyle = {
+      'background-color': 'rgba(255, 255, 255, 0.75)',
+      border: '1px solid #ced4da',
+      color: '#cc7832'
+    };
+
+    this.formStyle = {
+      color: '#437da8',
+      'background-color': 'rgba(255, 255, 255, 0.75)',
+      'backdrop-filter': 'blur(50px)',
+      'box-shadow': '0 2px 10px rgba(0, 0, 0, 0.075)'
+    };
+
+    this.inputStyle = {
+      color: '#282b2e',
+      'background-color': '#fff',
+      border: '1px solid #ced4da'
+    };
+  }
+
+  switchToDark(): void {
+    this.buttonStyle = {
+      'background-color': '#424242',
+      border: '1px solid #ced4da',
+      color: '#cc7832'
+    };
+
+    this.formStyle = {
+      color: '#437da8',
+      'background-color': 'rgba(34, 34, 34, 0.75)',
+      'backdrop-filter': 'blur(50px)',
+      'box-shadow': '0 2px 10px rgba(0, 0, 0, 0.075)'
+    };
+
+    this.inputStyle = {
+      color: '#282b2e',
+      'background-color': '#fff',
+      border: '1px solid #ced4da'
+    };
+  }
+}
