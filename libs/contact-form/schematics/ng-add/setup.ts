27a219e3331b83903dbcb9aaaca7938d729a61fd
tehw0lf <tehwolf@protonmail.com>
2022-08-26 12:45:36 +0200

    Bump flex fallback version

--- libs/contact-form/schematics/ng-add/setup.ts
+++ libs/contact-form/schematics/ng-add/setup.ts
@@ -14,7 +14,7 @@ import { Schema } from './schema';
 
 const contactFormModuleName = 'ContactFormModule';
 const contactFormPackageName = '@tehw0lf/contact-form';
-const flexLayoutFallbackVersion = '^13.0.0-beta.36';
+const flexLayoutFallbackVersion = '^14.0.0-beta.40';
 
 export default function (options: Schema): Rule {
   return async () => {
