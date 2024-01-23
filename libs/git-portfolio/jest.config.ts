a4fdcac1f59bab4fa6f0cb37b26af1e529960d2f
tehw0lf <tehwolf@protonmail.com>
2024-01-23 22:16:46 +0100

    Run migrations

--- libs/git-portfolio/jest.config.ts
+++ libs/git-portfolio/jest.config.ts
@@ -3,17 +3,18 @@ export default {
   displayName: 'git-portfolio',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$'
-    }
-  },
+  globals: {},
   collectCoverage: true,
   coverageReporters: ['json', 'html'],
   coverageDirectory: '../../coverage/libs/git-portfolio',
   transform: {
-    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular'
+    '^.+.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
   },
   transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
