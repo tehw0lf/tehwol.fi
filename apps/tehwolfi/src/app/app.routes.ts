810d120482c52df576be83e15c494b519fb4d868
tehw0lf <tehwolf@protonmail.com>
2024-01-23 23:06:32 +0100

    Migrate to standalone routing

--- apps/tehwolfi/src/app/app.routes.ts
+++ apps/tehwolfi/src/app/app.routes.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { GitPortfolioComponent } from './git-portfolio/git-portfolio.component';
 import { HomeComponent } from './home/home.component';
 import { WordlistGeneratorComponent } from './wordlist-generator/wordlist-generator.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "portfolio", component: GitPortfolioComponent },
   { path: "wordlist-generator", component: WordlistGeneratorComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- apps/tehwolfi/src/main.ts
+++ apps/tehwolfi/src/main.ts
@@ -1,28 +1,28 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { WordlistGeneratorModule } from '@tehw0lf/wordlist-generator';
-import { GitPortfolioModule } from '@tehw0lf/git-portfolio';
-import { ContactFormModule } from '@tehw0lf/contact-form';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatListModule } from '@angular/material/list';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideRouter } from '@angular/router';
+import { ContactFormModule } from '@tehw0lf/contact-form';
+import { GitPortfolioModule } from '@tehw0lf/git-portfolio';
+import { WordlistGeneratorModule } from '@tehw0lf/wordlist-generator';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 import { SidenavService } from './app/nav/sidenav.service';
+import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();
@@ -30,10 +30,11 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(AppRoutingModule, BrowserModule, DragDropModule, ReactiveFormsModule, ClipboardModule, MatCardModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatToolbarModule, MatSidenavModule, MatButtonModule, ContactFormModule, GitPortfolioModule, WordlistGeneratorModule),
+        importProvidersFrom(BrowserModule, DragDropModule, ReactiveFormsModule, ClipboardModule, MatCardModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatToolbarModule, MatSidenavModule, MatButtonModule, ContactFormModule, GitPortfolioModule, WordlistGeneratorModule),
         SidenavService,
         provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes)
     ]
 })
   .catch(err => console.error(err));
