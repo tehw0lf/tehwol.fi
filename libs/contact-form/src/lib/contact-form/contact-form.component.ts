710f1269668b588ec8873ba55aa6c80c2e7d2493
tehw0lf <tehwolf@protonmail.com>
2024-03-24 02:54:17 +0100

    Generate form dynamically with formly

--- libs/contact-form/src/lib/contact-form/contact-form.component.ts
+++ libs/contact-form/src/lib/contact-form/contact-form.component.ts
@@ -1,17 +1,25 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { AsyncPipe, NgStyle } from '@angular/common';
-import { Component, Input, OnDestroy, ViewEncapsulation } from '@angular/core';
 import {
-  ReactiveFormsModule,
-  UntypedFormBuilder,
-  UntypedFormControl,
-  UntypedFormGroup,
-  Validators
-} from '@angular/forms';
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil, tap } from 'rxjs/operators';
+
+interface FormConfigEntry {
+  field: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  value?: any;
+  required?: boolean;
+}
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -23,13 +31,14 @@ import { takeUntil, tap } from 'rxjs/operators';
   imports: [
     LayoutModule,
     ReactiveFormsModule,
+    FormlyModule,
     NgStyle,
     MatFormFieldModule,
     MatInputModule,
     AsyncPipe
   ]
 })
-export class ContactFormComponent implements OnDestroy {
+export class ContactFormComponent implements OnInit, OnDestroy {
   @Input() buttonStyle = {
     'background-color': '#333333',
     border: 'none',
@@ -49,37 +58,36 @@ export class ContactFormComponent implements OnDestroy {
 
   @Input() textStyle = { color: '#cc7832' };
 
-  @Input() nameLabel = 'Your Name';
-
-  @Input() emailAddressLabel = 'Your E-Mail Address';
-
-  @Input() messageLabel = 'Your Message';
-
   @Input() sendText = 'Send';
 
   @Input() sendSuccessfulText = 'E-Mail successfully sent';
 
   @Input() sendErrorText = 'Send error';
 
+  @Input() formConfig: FormConfigEntry[] = [
+    { field: 'name', required: true },
+    { field: 'email', required: true },
+    { field: 'message', required: true }
+  ];
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   @Input() apiCallback!: (formValue: any) => Observable<boolean>;
 
-  formGroup: UntypedFormGroup;
+  form = new FormGroup({});
+  fields: FormlyFieldConfig[] = [];
+  model: { [key: string]: string } = {};
+
   emailSent: Subject<boolean | null> = new Subject();
   emailSent$ = this.emailSent.asObservable();
 
   private unsubscribe$: Subject<void> = new Subject();
 
-  constructor(private builder: UntypedFormBuilder) {
+  constructor() {
     this.emailSent.next(null);
-    this.formGroup = this.builder.group({
-      name: new UntypedFormControl('', [Validators.required]),
-      email: new UntypedFormControl('', [
-        Validators.required,
-        Validators.email
-      ]),
-      message: new UntypedFormControl('', [Validators.required])
-    });
+  }
+
+  ngOnInit(): void {
+    this.buildConfig();
   }
 
   ngOnDestroy(): void {
@@ -87,12 +95,29 @@ export class ContactFormComponent implements OnDestroy {
     this.unsubscribe$.complete();
   }
 
-  submitFormData(formData: UntypedFormGroup) {
-    this.apiCallback(formData.value)
+  submitFormData(formData: { [key: string]: string }) {
+    this.apiCallback(formData)
       .pipe(
         tap((success: boolean) => this.emailSent.next(success)),
         takeUntil(this.unsubscribe$)
       )
       .subscribe();
+  }
+
+  private buildConfig(): void {
+    this.formConfig.forEach((entry: FormConfigEntry) => {
+      if (entry.value) {
+        this.model[entry.field] = entry.value;
+      }
+      this.fields.push({
+        key: entry.field,
+        type: 'input',
+        props: {
+          label: entry.field.toLocaleUpperCase(),
+          placeholder: 'Enter ' + entry.field,
+          required: true
+        }
+      });
+    });
   }
 }
